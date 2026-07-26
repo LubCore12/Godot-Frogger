79f90db910c1df30ce5171cb4f609101ec6edb93
Andrew Fomin <andrfom1234567810@gmail.com>
2026-07-26 22:45:52 +0700

    Added animations to the player

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -7,7 +7,18 @@ var speed: int = 100
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = speed * direction
+	animation()
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("confirm"):
 		print("something")
+		
+func animation():
+	if direction:
+		if direction.x:
+			$Animation.animation = "left"
+			$Animation.flip_h = direction.x > 0	
+		else:
+			$Animation.animation = "up" if direction.y < 0 else "down"
+	else:
+		$Animation.frame = 0
