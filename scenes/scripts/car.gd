fb411be524f91b89a82d3693b206307d8a2c803a
Andrew Fomin <andrfom1234567810@gmail.com>
2026-07-27 14:14:14 +0700

    Made level map

--- scenes/scripts/car.gd
+++ scenes/scripts/car.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 var direction: Vector2 = Vector2.LEFT
 var speed: int = 120
+var colors: Array = [
+	preload("res://graphics/cars/green.png"),
+	preload("res://graphics/cars/red.png"),
+	preload("res://graphics/cars/yellow.png")
+]
 
 func _ready() -> void:
+	$Sprite.texture = colors.pick_random()
 	if position.x < 0:
 		direction = Vector2.RIGHT
 		$Sprite.flip_h = true
